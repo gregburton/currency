a3a4dca710d43071a2730087b81ffb40c5887720
gregurton <gburt802@gmail.com>
2014-09-25 07:49:01 -0400

    commit 3

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,26 +1,43 @@
 class Currency
-  def run
-    fail "You need to add some functionality here before you can use this."
+  attr_reader: :amount, :code
+  def initialize(amount, code)
+
   end
 
-  # Should be created with an amount and a currency code
+  @amount = amount
+  @code = code
 
-  # Should equal another Currency object with the same amount and currency code
+  def ==(other)
+    if self.amount == other.amount && self.code == other.code
+      true
+  end
 
-  # Should be able to be added to another Currency object with the same currency code
   def +(other)
-    Currency.new(self.amount + other.amount, self.code)
+    if self.code != other.code
+      raise DifferentCurrencyCodeError, "The codes of these currencies differ. The operation cannot be completed unless they have the same code."
+    else
+      Currency.new(self.amount + other.amount, self.code)
+    end
   end
 
-  # Should be able to be subtracted by another Currency object with the same currency code
 
-  # Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
+  def -(other)
+    if self.code != other.code
+      raise DifferentCurrencyCodeError, "The codes of these currencies differ. The operation cannot be completed unless they have the same code."
+    else
+      Currency.new(self.amount - other.amount, self.code)
+    end
+  end
+
+  def mult_fixnum
+    
+  end
+
+
 
   # Should be able to be multiplied by a Fixnum or Float and return a Currency object
 
-  # Should be created with a Hash of currency codes to conversion rates (see below)
-
-  # At first, just make this work with two currency codes and conversion rates, with one rate being 1.0 and the other being the conversation rate (like this: {USD: 1.0, EUR: 0.74})
+@rates = { USD 1.0, CAD 1.11}
 
   # Should be able to take a Currency object and a requested currency code that is the same currency code as the Currency object's and return a Currency object equal to the one passed in (that is, currency_converter.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD))
 
